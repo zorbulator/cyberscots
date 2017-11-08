a8a23084459100c6ad7a7877b86121f2107bcd76
zorbulator <zorbulator@gmail.com>
2017-11-07 18:44:43 -0800

    Update smoothPageScroll.js

--- Smooth page scroll_files/smoothPageScroll.js
+++ Smooth page scroll_files/smoothPageScroll.js
@@ -4,8 +4,8 @@ $(function(){
 
 	var $window = $(window);		//Window object
 
-	var scrollTime = 0.25;			//Scroll time
-	var scrollDistance = 140;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+	var scrollTime = 0.3;			//Scroll time
+	var scrollDistance = 160;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
 
 	$window.on("mousewheel DOMMouseScroll", function(event){
 
